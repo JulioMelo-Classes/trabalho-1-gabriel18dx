c08d0f6b0d284c95fe6a2396712553a60b3d477d
juliocpmelo <julio.melo@imd.ufrn.br>
2021-07-14 19:51:36 -0300

    Adicionando correções e feedback

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,20 @@
 #include "Aposta.hpp"
 using namespace std;
 
+/*
+Avaliação
+Processamento do Arquivo de Entrada: 0,3 (não fez quando o arquivo é inexistente, não testou números repetidos, não testou formatação, não testou a presença de carateres estranhos)
+Execução correta: 0,3 (apenas faz o processamento inicial do arquivo)
+Interfcace Textual: 0,2 (apenas mostra o inico da interface)
+Documentação: 0
+Compilação automatizada: 1
+Organização em src, include, build, data: 1
+
+Comentários
+- Como está incompleto vou deixar sem muitos comentários... Acho que você começou bem com a classe Aposta (que deve ser mais ou menos a ideia da KenoBet).
+- Uma coisa que você poderia ter feito, independente da entrega das funcionalidades, era a documentação no formato doxygen.
+*/
+
 int main(int argc, char *argv[]) {
     // for(int i=0; i<argc; i++) {
     //     cout<<"Argumento["<<i<<"]"<<argv[i]<<endl;
